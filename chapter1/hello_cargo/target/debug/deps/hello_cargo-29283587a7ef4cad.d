5ecef9d539d63d8b12669a9064f663a4ff3429d3
Gianfranco <gdutk007@gmail.com>
2023-11-01 20:36:41 -0400

    first rust commit

---
+++ chapter1/hello_cargo/target/debug/deps/hello_cargo-29283587a7ef4cad.d
@@ -0,0 +1,5 @@
+/Users/gianfranco.dutka/rust/rustbook/chapter1/hello_cargo/target/debug/deps/hello_cargo-29283587a7ef4cad: src/main.rs
+
+/Users/gianfranco.dutka/rust/rustbook/chapter1/hello_cargo/target/debug/deps/hello_cargo-29283587a7ef4cad.d: src/main.rs
+
+src/main.rs:
---
+++ chapter1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/gianfranco.dutka/rust/rustbook/chapter1/hello_cargo/target/debug/hello_cargo: /Users/gianfranco.dutka/rust/rustbook/chapter1/hello_cargo/src/main.rs
